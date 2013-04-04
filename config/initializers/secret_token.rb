b331d337c854f2fcc154efd1de8979684b085bc4
Sean Smith <sean.smithy1@gmail.com>
2013-04-04 19:49:18 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWords::Application.config.secret_token = '4748bcbeeb8d925f2dc114f302e66a6c00d5316ee41b1d99475ddc140172f85f0388d188af3f343c61857e1bcb1458f4b031b2b67981d4b2e053ce9edf87c3b1'
