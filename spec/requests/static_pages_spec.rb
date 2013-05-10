6b4762d968668f7dd7a1abcfe55fa7b8434f3d1f
Sean Smith <sean.smithy1@gmail.com>
2013-05-10 10:48:57 +1000

    Start of users controller. Tests failing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
+	subject { page }
+
+  	shared_examples_for "all static pages" do
+    	it { should have_selector('h1',    text: heading) }
+    	it { should have_selector('title', text: full_title(page_title)) }
+  	end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
   end
 end
