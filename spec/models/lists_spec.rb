eedbfd9e2b3ad9e382d765415d794eb37701cdd7
Sean Smith <sean.smithy1@gmail.com>
2013-05-21 20:56:54 +1000

    Added some list testing

--- spec/models/lists_spec.rb
+++ spec/models/lists_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
-describe Lists do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe List do
+
+before do
+  @list = List.new(listname: "Example_List")
+  end
+
+subject { @list }
+
+  it { should respond_to(:listname) }
+
+
+  describe "when list is not present" do
+    before { @list.listname = " " }
+    it { should_not be_valid }
+  end
+ 
+  describe "when listname is too long" do
+    before { @list.listname = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+
 end
