00929f99b87e840f09aa3937e142dfb57660117f
Sean Smith <sean.smithy1@gmail.com>
2014-03-23 18:10:57 +1000

    resolved User bug - not updating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user,
-                only: [:index, :edit, :update, :destroy]
-  before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user,     only: [:destroy, :index]
+  before_filter :signed_in_user, only: [:edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy, :index]
 
   def show
     @user = User.find(params[:id])
@@ -17,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to MyWords!"
       redirect_to @user
     else
       render 'new'
@@ -25,14 +24,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    logger.debug "***---- I am here"
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
       redirect_to @user
     else
+      flash[:error] = "Profile not update"
       render 'edit'
     end
   end
@@ -55,7 +58,7 @@ class UsersController < ApplicationController
 private
 
   def correct_user
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
 
@@ -64,6 +67,6 @@ private
   end
 
   def user_params
-    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :name, :password, :password_confirmation, :admin)
   end
 end
